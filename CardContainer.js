ec03d280243bf0ff09dcfc9749b57ed154465e0f
Aidan McKay <aidanmckay2000@gmail.com>
2019-04-15 08:48:47 -0600

    Setting up the skeleton for my app layout

---
+++ CardContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Card from './Card.js'; 
+import './Header.scss';
+
+class CardContainer extends Component {
+    constructor() {
+        super()
+
+    }
+}
+
+export default CardContainer;
---
+++ Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Header.scss';
+
+
+class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
+            showCard: false
+        }
+    }
+}
